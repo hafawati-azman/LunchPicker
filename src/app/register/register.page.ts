48445296bbd1ad2acc304ebec93d2450020fa95e
Raja Noor Hafawati <raja.hafawati@mycit.ie>
2019-07-15 21:39:13 +0800

    150719: User registration page and database: done.

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Validators, FormGroup, FormBuilder, FormControl } from "@angular/forms";
+import { ToastController } from '@ionic/angular';
+import { async } from 'q';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -13,7 +16,9 @@ export class RegisterPage implements OnInit {
 
   constructor(
     private http: HttpClient, 
-    public formBuilder: FormBuilder
+    public formBuilder: FormBuilder,
+    public toastController: ToastController,
+    private router: Router
   ) { 
     this.registerForm = this.formBuilder.group({
       user_name: new FormControl(''),
@@ -25,18 +30,25 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
-  //registerUser() {
-    //let data = {
-      //user_name: this.registerForm.value.user_name,
-      //user_email: this.registerForm.value.user_email,
-      //user_password: this.registerForm.value.user_password,
-    //}
-    //this.http.post('http://127.0.0.1/register.php', data).subscribe(
-      //(result) => {
-        //console.log(result);
-      //},
-      //(err) => {
-        //console.log(JSON.stringify(err));
-      //});}
+  registerUser() {
+    let data = {
+      user_name: this.registerForm.value.user_name,
+      user_email: this.registerForm.value.user_email,
+      user_password: this.registerForm.value.user_password,
+    }
+    this.http.post('http://127.0.0.1/lp_registeruser.php', data).subscribe(
+      async (result) => {
+        console.log(result);
+        ///this.router.navigate['/login']
+        const toast = await this.toastController.create({
+          message: 'Congratulations, you may now login!',
+          duration: 2000
+         });
+        toast.present();
+
+      },
+      (err) => {
+        console.log(JSON.stringify(err));
+      });}
 
 }
